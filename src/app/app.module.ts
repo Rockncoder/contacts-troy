f52c46155136b07f8d1c9bbc87bb7d40a8a99180
rockncoder <rockncoder@gmail.com>
2017-02-26 14:51:17 -0800

    Adds contacts.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {AngularFireModule} from 'angularfire2';
+
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
@@ -11,6 +13,14 @@ import { AboutComponent } from './about/about.component';
 
 import {ContactService} from './contact.service';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCks_pUoLT9q8MRA42WCbZLLtwdnuHIhY8",
+  authDomain: "contacts-troy.firebaseapp.com",
+  databaseURL: "https://contacts-troy.firebaseio.com",
+  storageBucket: "contacts-troy.appspot.com",
+  messagingSenderId: "816795509968"
+};
 
 @NgModule({
   declarations: [
@@ -23,7 +33,8 @@ import { ContactDetailsComponent } from './contact-details/contact-details.compo
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [ContactService],
   bootstrap: [AppComponent]
